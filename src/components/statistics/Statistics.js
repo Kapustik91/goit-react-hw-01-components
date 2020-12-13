31f6d06d1c6352405e6855ef30698e20fdf77d5a
Yuliia Kovalenko <kus_queen@icloud.com>
2020-12-13 13:28:46 +0200

    updates

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -6,7 +6,7 @@ import randomColor from './randomColor';
 export default function Statistics({ title, stats }) {
     return (
         <section className={styles.statistics} >
-         {title && <h2 className={styles.title}>Upload stats</h2>}
+            {title && <h2 className={styles.title}>{title}</h2>}
             <ul className={styles.statList}>
                 {stats.map(stat => (
                     <li className={styles.item}
